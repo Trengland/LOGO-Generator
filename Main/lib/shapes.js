5111d79a332b8d49d131a3ac5b5d655c592c37f5
Trengland <englandvivial@gmail.com>
2023-04-06 17:21:29 -0400

    included working test files for shapes.js and svg.js

--- Main/lib/shapes.js
+++ Main/lib/shapes.js
@@ -13,7 +13,7 @@ class Circle extends Shape {
 }
 
 class Square extends Shape {
-    render() {return `<rect x="90" v="40" width="120" height="120" fill="${this.color}" />`;
+    render() {return `<rect x="90" y="40" width="120" height="120" fill="${this.color}" />`;
     }
 }
 
