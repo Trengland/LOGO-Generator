35829730f56d3a15e8955a69e8be8761fc341ad2
Trengland <englandvivial@gmail.com>
2023-04-06 17:50:59 -0400

    complete

--- Main/lib/shapes.js
+++ Main/lib/shapes.js
@@ -18,7 +18,7 @@ class Square extends Shape {
 }
 
 class Triangle extends Shape {
-    render() {return `polygon points="150, 18, 244, 182, 56, 182" fill="${this.color}" />`;
+    render() {return `<polygon points="150, 18, 244, 182, 56, 182" fill="${this.color}" />`;
     }
 }
 
